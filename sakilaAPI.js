eca4c5a979236fbf9d23cd53566734385cb92981
anitachandlermohan <45198039+anitachandlermohan@users.noreply.github.com>
2019-01-17 17:50:33 +0000

    Add files via upload

--- sakilaAPI.js
+++ sakilaAPI.js
@@ -4,7 +4,6 @@
 function getAll(){
     let xhttp = new XMLHttpRequest();
     let url = "http://localhost:8080/api/film_list";
-    let film_list_array = [];
     let film_table = document.createElement("TABLE");
     let table_body = document.createElement("TBODY");
     xhttp.onreadystatechange = function (){
@@ -13,19 +12,24 @@ function getAll(){
             let film_list = JSON.parse(xhttp.responseText);
             for(let i in film_list){
                 let row = document.createElement("TR");
+                // title column //
                 let title_cell = document.createElement("TD");
                 let title_cell_text = document.createTextNode( JSON.stringify(film_list[i].title));
                 title_cell.appendChild(title_cell_text);
                 row.appendChild(title_cell);
+
+                // description column //
+                let desc_cell = document.createElement("TD");
+                let desc_cell_text = document.createTextNode(JSON.stringify(film_list[i].description));
+                desc_cell.appendChild(desc_cell_text);
+                row.appendChild(desc_cell);
+
+                //append this row to table// 
                 table_body.appendChild(row);
-                
-                
+
             }
-    
-            
+
         }
-   
- 
        
     }
     
@@ -36,6 +40,26 @@ function getAll(){
     
    
 }   
+
+function getFilmByID(){
+    // document.getElementById("film_list").removeChild(document.getElementById("film_list").lastChild);
+    let filmID = document.getElementById("inputFID").value;
+    let xhttp = new XMLHttpRequest();
+    url = "http://localhost:8080/api/film_list/" + filmID;
+    
+    xhttp.onreadystatechange = function (){
+        if (xhttp.readyState == 4 && xhttp.status == 200){
+
+            let film_list = JSON.parse(xhttp.responseText);
+            console.log(JSON.stringify(film_list));
+            let search_result = document.createTextNode(JSON.stringify(film_list));
+            document.getElementById("filmlist").appendChild(search_result);
+        }
+    }
+    xhttp.open("GET", url,true);
+    xhttp.send();
+    
+}
  
 // function showFilmNames(data){
 //     let output = "<ul>";
